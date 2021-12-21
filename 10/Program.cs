b0e06a88302a3264fd28b4f6a7d08f4b82df0923
Karrighun <92940822+Karrighun@users.noreply.github.com>
2021-12-22 03:07:07 +0500

    Revert "Intern"

--- 10/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿//Показать вторую цифру трёхзначного числа
-void SecondSymbol(int number)
-{
-    Console.WriteLine(number / 10 % 10);
-}
-SecondSymbol(1341235);
-
-//done
--- 12/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Удалить вторую цифру трёхзначного числа
-void DeleteSecondSymbol(int number)
-{
-    Console.WriteLine((number / 100) * 10 + (number % 10));
-}
-DeleteSecondSymbol(543);
-
-//done
