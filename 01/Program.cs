99fafeec0cacb4827d08006207dd46e4d67b667e
Karrighun <danilgaz@yandex.ru>
2021-12-22 02:57:07 +0500

    intern objeсtives added

---
+++ 01/Program.cs
@@ -0,0 +1,19 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите два числа, которые хотите проверить");
+int i = Convert.ToInt32 (Console.ReadLine());
+int j = Convert.ToInt32 (Console.ReadLine());
+if(i == j * j) Console.WriteLine($"{i} Являеется квадратом числа {j}");
+ else Console.WriteLine($"{i} Не являеется квадратом числа {j}");
+
+// OR
+
+bool IsSquare(int x, int y)
+{
+    if (x == y * y) return true;
+    else
+        return false;
+}
+Console.WriteLine(IsSquare(25, 5));
+
+//done
---
+++ 09/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+void LastSymbol(int number)
+{
+    Console.WriteLine(number % 10);
+}
+LastSymbol(115);
+
+//done
