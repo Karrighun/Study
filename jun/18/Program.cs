809b6e42020e7c923350593406fd0e702e713b8c
Karrighun <danilgaz@yandex.ru>
2021-12-28 00:19:38 +0500

    jun folder added

---
+++ jun/18/Program.cs
@@ -0,0 +1,14 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool CheckStatement(bool X, bool Y)
+{
+    if (!(X || Y) == (!X && !Y))
+        return true;
+    else
+        return false;
+}
+
+if (CheckStatement(false, false) && CheckStatement(true, false) && CheckStatement(false, true) && CheckStatement(true, true))
+Console.WriteLine("Да");
+else
+Console.WriteLine("нет");
---
+++ jun/20/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+string QuaterRange(int quarter)
+{
+    string[] range = new string[] { "X in (-inf, 0); Y in (0, +inf)", "X in (0, +inf); Y in (0, +inf)", "X in (0, +inf); Y in (-inf, 0)", "X in (-inf, 0); Y in (-inf, 0)" };
+    return range[quarter - 1];
+}
+Console.WriteLine(QuaterRange(1));
